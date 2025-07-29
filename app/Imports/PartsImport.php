c83fe0e7345f84492f20d7377ca419e84af72540
OnlyAbdullh <abdallaalkasm9@gmail.com>
2025-07-29 11:41:50 +0300

    make the import excel parts start with raw 2.

--- app/Imports/PartsImport.php
+++ app/Imports/PartsImport.php
@@ -4,14 +4,19 @@ namespace App\Imports;
 
 use App\Models\Part;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PartsImport implements ToModel
+class PartsImport implements ToModel,WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    public function startRow(): int
+    {
+        return 2;
+    }
     public function model(array $row)
     {
         return new Part([
