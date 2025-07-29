26f06b4d36f7b55bf3e2ec7e688cc4e5340c308e
OnlyAbdullh <abdallaalkasm9@gmail.com>
2025-07-29 12:36:44 +0300

    make the min quantity is 0 instead of 1.

--- app/Http/Requests/StoreReportRequest.php
+++ app/Http/Requests/StoreReportRequest.php
@@ -51,7 +51,7 @@ class StoreReportRequest extends FormRequest
 
             'parts_used'                     => 'sometimes|array|min:1',
             'parts_used.*.part_id'           => 'sometimes|nullable|integer|exists:parts,id',
-            'parts_used.*.quantity'          => 'sometimes|nullable|integer|min:1',
+            'parts_used.*.quantity'          => 'sometimes|nullable|integer|min:0',
             'parts_used.*.notes'             => 'nullable|string',
             'parts_used.*.is_faulty'         => 'sometimes|nullable|boolean',
             'parts_used.*.faulty_quantity'   => 'sometimes|nullable|integer|min:1',
